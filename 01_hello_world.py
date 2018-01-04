23463f9a8a54c82d4f5f4660715038c5936779e9
sddz_yuxiaohua <sddz_yuxiaohua@163.com>
2018-01-04 22:06:12 +0800

    add boolean and datetime class introduction

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -25,6 +25,9 @@ a = 28 # this is a perfect number
 print type(a)
 # <type 'int'>
 
+x = long(28)
+y = float(29)
+
 import sys
 
 b = sys.maxint + 1
@@ -46,3 +49,47 @@ print type(c)
 # <type 'complex'>
 
 print c.real, c.imag
+
+# 3. booleans
+
+# Only two values: True, False
+
+type(True) # type(False)
+# <type 'bool'>
+
+a = True + 100
+# 101
+b = False * 100
+# 0
+
+# 4. datetime
+
+import datetime
+
+gvr = datetime.date(1986, 7, 11)
+
+print gvr.year  # 1986
+print gvr.month # 7
+print gvr.day   # 11
+print gvr.ctime()   # Fri Jul 11 00:00:00 1986
+
+launch_date = datetime.date(2017, 3, 30)
+launch_time = datetime.time(22, 27, 0)
+launch_datetime = datetime.datetime(2017, 3, 30, 22, 27, 0)
+
+print launch_date   # 2017-03-30
+print launch_time   # 22:27:00
+print launch_datetime   # 2017-03-30 22:27:00
+
+print datetime.datetime.today() 
+# datetime.datetime(2018, 1, 4, 21, 54, 10, 808000)
+
+# 4.1 convert string to datetime:
+
+# - Module: datetime
+# - Class: datetime
+# - Method: strptime()
+
+moon_landing = "7/20/1969"
+moon_landing_datetime = datetime.datetime.strptime(moon_landing, "%m/%d/%Y")
+# 1969-07-20 00:00:00
